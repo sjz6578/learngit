4912b029d17e94882d4f9ee7181f1785bc275ba2
test2 <test2@test.com>
2014-11-11 08:19:12 +0800

    add middle in a.c

---
+++ a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv)
+{
+        char buffer[96];
+        printf("- %p -\\n", &buffer);
+        strcpy(buffer, getenv("KIRIKA"));
+	printf("add one line in middle\n");
+        return 0;
+}
